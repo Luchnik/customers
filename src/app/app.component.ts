d77872633e6b4c2d30fca8e636e5083119d85edc
Andriy Luchko <andruluchko@ukr.net>
2017-02-15 09:58:44 +0200

    added customer class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,29 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'my-app',
-  template: `
-    <h1>{{name}}</h1>
-    <p><i>{{name}} is in the {{region}} region</i></p>
-    <button (click)="addressClick()">Add/Show address</button>
-    <div [hidden]="hideAddress">
-      <fieldset>
-        <label>Street: </label> {{street}}
-      </fieldset>
-      <fieldset>
-        <label>City</label> {{city}}
-      </fieldset>
-      <fieldset>
-        <label>Region</label>
-        <select (change)="regionChange($event.target.value)">
-          <option>east</option>
-          <option>north</option>
-          <option>south</option>
-          <option>west</option>
-        </select>
-      </fieldset>
-    </div>
+import { Customer } from './model';
 
-    <fieldset>
-      <img [src]="image" alt="favicon">
-    </fieldset>
-    <label [style.color]="color">Favourite color</label> {{color}}
-    <button (click)="clicked()">Change color</button>
-    <select (change)="changeColor($event.target.value)">
-      <option>red</option>
-      <option>blue</option>
-      <option>green</option>
-    </select>
-  `,
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent { 
-  name = 'Alex Smith';
-  image = 'favicon.ico';
-  color = 'red';
-  street = '19 Karmanskogo';
-  city = 'Lviv';
-  region = 'east';
   hideAddress = false;
 
-  clicked() {
-    this.color = this.color === 'red' ? 'blue' : 'red';
-  }
+  //customer: Customer = new Customer();
+  customer: Customer = {
+    id: 1,
+    name: 'Alex Price',
+    address: {
+      street: '19 Karmanskogo',
+      city: 'Lviv',
+      region: 'east',
+      state: 'California'
+    }
+  };
 
-  changeColor(color: string) {
-    this.color = color;
-  }
-
+  /*
   addressClick() {
     this.hideAddress = !this.hideAddress;
   }
@@ -60,4 +31,5 @@ export class AppComponent {
   regionChange(region: string) {
     this.region = region;
   }
+  */
 }
